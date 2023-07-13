5586a3482c4f65d8fd2be531d93ccd22a0ba499c
ntr <ntr@ihfood.dk>
2023-07-13 13:02:17 +0200

    update pack script to use workflow and try to parallize it since now a lot of packages

--- all-pack.ps1
+++ all-pack.ps1
@@ -1,8 +1,23 @@
+#!/usr/bin/env pwsh
 ./getnuget.ps1
-$nuspecs = gci -Recurse ./defs/*.nuspec
-ForEach ($nuspec in $nuspecs)
-{
-   $command = "./pkgs/nuget.exe pack $nuspec -OutputDirectory ./pkgs/"
-   write-host $command
-   iex $command
+
+$nuspecs = Get-ChildItem -Recurse ./defs/*.nuspec
+$nugetExePath = Join-Path $PSScriptRoot "pkgs/nuget.exe"
+$outputDirectory = Join-Path $PSScriptRoot "pkgs/"
+
+# Use workflow to try to parallize nuget packs
+workflow PackNuspecs {
+    param($nugetExePath, $nuspecs, $outputDirectory)
+    
+    foreach -parallel ($nuspec in $nuspecs) {
+        InlineScript {
+            $nuspecPath = $using:nuspec.FullName
+            $outputDir = $using:outputDirectory
+            $command = "$using:nugetExePath pack $nuspecPath -OutputDirectory $outputDir"
+            Write-Output "Executing command: $command"
+            & $using:nugetExePath pack $nuspecPath -OutputDirectory $outputDir
+        }
+    }
 }
+
+PackNuspecs -nugetExePath $nugetExePath -nuspecs $nuspecs -outputDirectory $outputDirectory
--- getnuget.ps1
+++ getnuget.ps1
@@ -1,6 +1,8 @@
+#!/usr/bin/env pwsh
+# win-x86 only for now (download manually to directory and this will be skipped)
 $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-# We download into packages so it is not checked-in since this is ignored in git
-$packagesPath = ".\pkgs\"
+# Download into specific directory so it is not checked-in since this is ignored in git
+$packagesPath = "./pkgs/"
 $targetNugetExe = $packagesPath + "nuget.exe"
 # Download if it does not exist
 If (!(Test-Path $targetNugetExe))
