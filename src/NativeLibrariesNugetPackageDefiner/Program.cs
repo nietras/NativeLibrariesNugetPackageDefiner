68cc604d1c5987b7aef3cd1be0202581cf62a10c
ntr <ntr@ihfood.dk>
2023-07-18 11:32:52 +0200

    add System.CommandLine and use input/output arguments

--- src/NativeLibrariesNugetPackageDefiner/Program.cs
+++ src/NativeLibrariesNugetPackageDefiner/Program.cs
@@ -1,14 +1,28 @@
 using System;
+using System.CommandLine;
 using System.Diagnostics;
 using static NativeLibrariesNugetPackageDefiner.NugetPackageDefiner;
 
 Action<string> log = t => { Trace.WriteLine(t); Console.WriteLine(t); };
 
-var baseDirectory = "../../../../../";
-var nativeLibsForPackageDirectory = baseDirectory + "libs";
-var outputDirectory = baseDirectory + "defs";
-// Native library files only so choosing very low TFM
-var targetFrameworkMoniker = ".netstandard1.1";
+var inputDirectoryArgument = new Argument<string>(
+    name: "input", description: "The directory to scan and define packages for.");
 
-FindNativeLibrariesThenDefinePackages(nativeLibsForPackageDirectory,
-    targetFrameworkMoniker, outputDirectory, log);
+var outputDirectoryArgument = new Argument<string>(
+    name: "output", description: "The directory to output package definitions in.");
+
+var rootCommand = new RootCommand("Native libraries nuget package definer.");
+rootCommand.AddArgument(inputDirectoryArgument);
+rootCommand.AddArgument(outputDirectoryArgument);
+
+rootCommand.SetHandler((inputDirectory, outputDirectory) =>
+    {
+        // Native library files only so choosing very low TFM
+        var targetFrameworkMoniker = ".netstandard1.1";
+
+        FindNativeLibrariesThenDefinePackages(inputDirectory,
+            targetFrameworkMoniker, outputDirectory, log);
+    },
+    inputDirectoryArgument, outputDirectoryArgument);
+
+return await rootCommand.InvokeAsync(args);
